07c5bb7a9438f3ee1fb100343beaad98dd157dea
renydiaz02 <reny_diaz@ucne.edu.do>
2021-10-05 23:54:37 -0400

    Creando los eventos de consulta

--- P1-AP1-Reny-20190003/MainWindow.xaml.cs
+++ P1-AP1-Reny-20190003/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using P1_AP1_Reny_20190003.UI.Consulta;
+using P1_AP1_Reny_20190003.UI.Registro;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,18 @@ namespace P1_AP1_Reny_20190003
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            rAportes r = new rAportes();
+            r.Show();
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            cConsulta c = new cConsulta();
+            c.Show();
         }
     }
 }
--- P1-AP1-Reny-20190003/UI/Consulta/cAportes.xaml.cs
+++ P1-AP1-Reny-20190003/UI/Consulta/cAportes.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using P1_AP1_Reny_20190003.BLL;
+using P1_AP1_Reny_20190003.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +24,41 @@ namespace P1_AP1_Reny_20190003.UI.Consulta
         public cConsulta()
         {
             InitializeComponent();
+        }
+
+        public static string Cantidad = "";
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var listado = new List<Aportes>();
+            if (CriterioTextBox.Text.Trim().Length > 0)
+            {
+                switch (FiltroComboBox.SelectedIndex)
+                {
+                    case 0: //Persona
+                        listado = AportesBLL.GetList(e => e.Persona.ToLower().Contains(CriterioTextBox.Text.ToLower()));
+                        break;
+                    case 1: //Concepto
+                        listado = AportesBLL.GetList(e => e.Concepto.ToLower().Contains(CriterioTextBox.Text.ToLower()));
+                        break;
+                }
+            }
+            else
+            {
+                listado = AportesBLL.GetList(e => true);
+            }
+
+            if (DesdeDataPicker.SelectedDate != null)
+                listado = AportesBLL.GetList(c => c.Fecha.Date >= DesdeDataPicker.SelectedDate);
+
+            if (HastaDataPicker.SelectedDate != null)
+                listado = AportesBLL.GetList(c => c.Fecha.Date <= HastaDataPicker.SelectedDate);
+
+            DatosDataGrid.ItemsSource = null;
+            DatosDataGrid.ItemsSource = listado;
+
+            Cantidad = listado.Count().ToString();
+            CantidadTextBlock.Text = Cantidad;
         }
     }
 }
