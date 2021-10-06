f37e249544b8559d5e3df11789e2898b1f214054
renydiaz02 <reny_diaz@ucne.edu.do>
2021-10-05 22:58:34 -0400

    Creando la entidad

---
+++ P1-AP1-Reny-20190003/Entidades/Aportes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1_AP1_Reny_20190003.Entidades
+{
+    public class Aportes
+    {
+        [Key]
+        public int aporteId { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        public string Persona { get; set; }
+        public string Concepto { get; set; }
+        public string Monto { get; set; }
+        public int Conteo { get; set; }
+        public double Total { get; set; }
+    }
+}
