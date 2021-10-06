bde925828ed606575d057f6a8bf999cfd722382b
renydiaz02 <reny_diaz@ucne.edu.do>
2021-10-05 23:25:03 -0400

    Creando al Ventana de registro

---
+++ P1-AP1-Reny-20190003/UI/Registro/rAportes.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace P1_AP1_Reny_20190003.UI.Registro
+{
+    /// <summary>
+    /// Interaction logic for rAportes.xaml
+    /// </summary>
+    public partial class rAportes : Window
+    {
+        public rAportes()
+        {
+            InitializeComponent();
+        }
+
+        private void Buscar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Nuevo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Guardar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Eliminar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
